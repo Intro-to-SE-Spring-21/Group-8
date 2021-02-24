39f08d73cd4988749df68eb138f1a278283e6b8c
lmurdock12 <44686596+lmurdock12@users.noreply.github.com>
2021-02-24 09:56:46 -0600

    Make follow button work

--- Account/views.py
+++ Account/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from Account.models import Follow
+from django.urls import reverse
 
 def register(request):
 
@@ -26,7 +27,36 @@ def register(request):
 
 def profile(request, username):
 
-    
+    if request.method == "POST":
+        
+        if not request.user.is_authenticated:
+            #If the user tries to POST anything and they are not logged in, redirect them to the login page
+            #at some point look at the thing that will redirect them back to the same page and complete the previous
+            #action once they are logged in
+            return HttpResponseRedirect(reverse('Account:login'))
+
+        if request.POST['followProfileSubmit']:
+            print(request.POST)
+
+            #Create query
+            #user = current session user
+            #following: is the user profile
+            userToFollow = User.objects.get(username=request.POST['followProfileSubmit'])
+            
+            
+            #make sure relationship does not already exist
+            old_follow = Follow.objects.filter(user=request.user,following=userToFollow)
+            #Check to make sure they are not equal
+            if(userToFollow.username != request.user.username and not old_follow):
+                print("Successfully meet criteria to follow")
+                query = Follow(user=request.user,following=userToFollow)
+                query.save()
+                #save the query
+                
+        #reload the page and make sure an unfollow button shows up 
+        return HttpResponseRedirect(reverse('Account:profile', args=[request.POST['followProfileSubmit']]))
+            
+
     user = get_object_or_404(User,username=username)
     following = len(Follow.objects.filter(following = user))
     return render(request,'Account/profile.html',{'user':user, 'following_count':following})
