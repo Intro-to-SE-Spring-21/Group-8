2ba00cf2ce18c32a4ecaa7867011151505c4b0f0
bhball22 <krazybill22@gmail.com>
2021-02-26 17:18:52 -0600

    Database Population including Posts

--- Account/management/commands/populateDatabase.py
+++ Account/management/commands/populateDatabase.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from Account.models import Follow
+from Account.models import Tweet
 import os
 
 
@@ -73,4 +74,23 @@ class Command(BaseCommand):
 
                 follow = Follow(user=user_following,following=value[i])
                 follow.save()
-        
+
+
+
+
+        post_1 = Tweet.objects.create(tweet_creator=User.objects.get(pk=11), tweet_text="Recent extreme weather events are a stark reminder that we’re already seeing the effects of climate change here at home and around the world. This type of observation system will help scientists monitor and predict future weather events.", pub_date='2021-02-26 10:30:00.000000')
+        post_2 = Tweet.objects.create(tweet_creator=User.objects.get(pk=11), tweet_text="To avoid a climate disaster, we need to eliminate emissions from the ways we create electricity, grow food, make things, move around, and heat and cool our buildings. It won’t be easy, but I believe we can do it. This book is about what it will take.", pub_date='2021-02-14 09:17:00.000000')
+        post_3 = Tweet.objects.create(tweet_creator=User.objects.get(pk=10), tweet_text="I want a chance to do for you what you do for me! #QueenSugar", pub_date='2021-02-16 08:00:00.000000')
+        post_4 = Tweet.objects.create(tweet_creator=User.objects.get(pk=10), tweet_text="The path to a stronger, healthier life, begins with the love we bring ourselves. Which is why this Valentine’s Day weekend, @ww_us and I are hosting Oprah’s Your Life In Focus: Be The Love You Need, a live virtual experience that will help you activate the life you most desire.", pub_date='2021-02-4 08:16:00.000000')
+        post_5 = Tweet.objects.create(tweet_creator=User.objects.get(pk=7), tweet_text="Some of our favorite moments from the #GitHubUniverse keynote. See the full version and all sessions, now on demand, githubuniverse.com", pub_date='2020-12-14 11:02:00.000000')
+        post_6 = Tweet.objects.create(tweet_creator=User.objects.get(pk=9), tweet_text="Congrats on 20 years!", pub_date="2021-02-22 16:15:00.000000")
+        post_7 = Tweet.objects.create(tweet_creator=User.objects.get(pk=9), tweet_text="Like a good neighbor @JakeSateFarm #ad", pub_date='2021-02-07 6:46:00.000001')
+        post_8 = Tweet.objects.create(tweet_creator=User.objects.get(pk=8), tweet_text='"This is a nice place. My mother\'d like a place like this. She likes these quiet little places where the water plays."\nFrom The Joy of Painting Series 12 Episode 8', pub_date="2021-02-26 16:01:00.000000")
+        post_9 = Tweet.objects.create(tweet_creator=User.objects.get(pk=8), tweet_text="Registration ends March 1 for the Bob Ross Run for the Trees Virtual 5K! International participants are welcome. Every step counts! All proceeds support Happy Little Tree planting and preservation efforts in Michigan state parks.Register now at https://bit.ly/2KVb2HU", pub_date="2021-02-26 21:45:00.000000")
+        post_10 = Tweet.objects.create(tweet_creator=User.objects.get(pk=6), tweet_text="That's it for our Virtual Student Organization Fair. If you missed any videos, you can check out the #BCoEvirtualorgfair hashtag. Or see the complete playlist over on our YouTube channel. Be sure to hit that subscribe button while you're there.", pub_date="2021-09-25 15:30:00.000000")
+        post_11 = Tweet.objects.create(tweet_creator=User.objects.get(pk=6), tweet_text="Engineering alumni are well-represented on the @MSU_Foundation board of directors. #WeRingTrue #HailState", pub_date="2021-02-26 14:47:00.00000")
+        post_12 = Tweet.objects.create(tweet_creator=User.objects.get(pk=3), tweet_text="Congratulations, we were told diddly squat about this project! #Confusion", pub_date="2021-01-25 15:33:25.16748")
+        post_13 = Tweet.objects.create(tweet_creator=User.objects.get(pk=4), tweet_text="It is definitely not February but it's not not February :)", pub_date="2021-02-28 12:00:00.000000")
+        post_14 = Tweet.objects.create(tweet_creator=User.objects.get(pk=5), tweet_text="And the Lord spake, saying, 'First shalt thou take out the Holy Pin. Then shalt thou count to three, no more, no less. Three shall be the number thou shalt count, and the number of the counting shall be three. Four shalt thou not count, neither count thou two, excepting that thou", pub_date="2021-02-14 11:59:59.999999")
+        post_15 = Tweet.objects.create(tweet_creator=User.objects.get(pk=5), tweet_text="then proceed to three. Five is right out. Once the number three, being the third number, be reached, then lobbest thou thy Holy Hand Grenade of Antioch towards thy foe, who, being naughty in My sight, shall snuff it.'\nBook of Armaments, chapter 2: 13-21", pub_date="2021-02-14 12:00:00.000000")
+        post_16 = Tweet.objects.create(tweet_creator=User.objects.get(pk=4), tweet_text="Happy Birthday", pub_date="1999-05-28 08:03:59.500000")
