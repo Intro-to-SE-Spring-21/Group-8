af24cd406dffed7d545ff4fe6bcc82508b782ec4
kd766 <kd766@msstate.edu>
2021-02-26 18:27:20 -0600

    Updated views.py(broken)

--- Account/views.py
+++ Account/views.py
@@ -30,6 +30,13 @@ def register(request):
 
 def profile(request, username):
 
+    profile_user = get_object_or_404(User,username=username)
+    #How many users is the profile user following
+    following = Follow.objects.filter(user = profile_user)
+    #how many people are following the profile user
+    followed_by = Follow.objects.filter(following=profile_user)
+
+
     if request.method == "POST":
         
         if not request.user.is_authenticated:
@@ -38,12 +45,12 @@ def profile(request, username):
             #action once they are logged in
             return HttpResponseRedirect(reverse('Account:login'))
 
-        if request.POST.get('followProfileSubmit'):
+        if request.POST.get('follow' + profile_user.username):
 
             #Create query
             #user = current session user
             #following: is the user profile
-            userToFollow = User.objects.get(username=request.POST['followProfileSubmit'])
+            userToFollow = User.objects.get(username=request.POST['follow' + profile_user.username])
             
             
             #make sure relationship does not already exist
@@ -55,27 +62,22 @@ def profile(request, username):
                 query.save()
                 #save the query
 
-            return HttpResponseRedirect(reverse('Account:profile', args=[request.POST['followProfileSubmit']]))
+            return HttpResponseRedirect(reverse('Account:profile', args=[request.POST['follow' + profile_user.username]]))
 
-        if request.POST.get('unfollowProfileSubmit'):
+        if request.POST.get('unfollow' + profile_user.username):
             
-            userToUnfollow = User.objects.get(username=request.POST['unfollowProfileSubmit'])
+            userToUnfollow = User.objects.get(username=request.POST['unfollow' + profile_user.username])
 
             #Relationship should already exist.
             unfollow = Follow.objects.filter(user=request.user,following=userToUnfollow)
             unfollow.delete()
        
             #reload the page and make sure an follow button shows back up
-            return HttpResponseRedirect(reverse('Account:profile', args=[request.POST['unfollowProfileSubmit']]))
+            return HttpResponseRedirect(reverse('Account:profile', args=[request.POST['unfollow' + profile_user.username]]))
         
             
 
-    profile_user = get_object_or_404(User,username=username)
-    #How many users is the profile user following
-    following = Follow.objects.filter(user = profile_user)
-    #how many people are following the profile user
-    followed_by = Follow.objects.filter(following=profile_user)
-
+    
     #if current_user is in followed_by...show unfollow
 
     #Check to see if we are on the users native profile if they are logged in
@@ -99,12 +101,33 @@ def profile(request, username):
             temp = random.choice(AllUsers)
         rand_three.append(temp)
 
+
+    list_follow = [False, True, True]
+    if request.user.is_authenticated:
+
+        #Check to see if the authenticated user is already following the user
+        for followed in followed_by:
+            if request.user == followed.user and followed.user == rand_three[0]:
+                list_follow[0] = True
+            if request.user == followed.user and followed.user == rand_three[1]:
+                list_follow[1] = True
+            if request.user == followed.user and followed.user == rand_three[2]:
+                list_follow[2] = True
+
+
     context = {'validSession':False, 'username':request.user.username, 'userdic':AllUsers, 'userlist':rand_three, 
     'profile_user':profile_user,'auth_follow':auth_follow, 'following_len':len(following),'followed_by_len':len(followed_by),
-    'isNative':isNative}
+    'isNative':isNative, 'list_follow':list_follow}
     
     if(request.user.is_authenticated):
         context['validSession'] = True
     
     return render(request,'Account/profile.html', context)
 
+
+#@register.filter(name='return_item')
+#def return_item(l, i):
+#    try:
+#        return l[i]
+#    except:
+#        return None
