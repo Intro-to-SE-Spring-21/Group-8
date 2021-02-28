68dff71a5caa78d2d1d4adfb181536d7208b53f9
bhball22 <krazybill22@gmail.com>
2021-02-28 10:30:22 -0600

    Syntax Fix Followers/ing number Home Page #53

--- Main_Page/views.py
+++ Main_Page/views.py
@@ -14,8 +14,7 @@ from django.urls import reverse
 
 def index(request):
 
-    #username = request.user.username
-    #profile_user = get_object_or_404(User,username=username)
+    profile_user = get_object_or_404(User,username=request.user.username)
 
     ## Creating a Tweet through the webpage
     if request.method == "POST":
@@ -41,17 +40,15 @@ def index(request):
             temp = random.choice(AllUsers)
         rand_three.append(temp)
 
-    #profile_user = request.user.username
-    #profile_user = get_object_or_404(User,username=username)
     #How many users is the profile user following
-    #following = Follow.objects.filter(user = profile_user)
+    following = Follow.objects.filter(user = profile_user)
     #how many people are following the profile user
-    #followed_by = Follow.objects.filter(following=profile_user)
+    followed_by = Follow.objects.filter(following=profile_user)
 
     ### Variable declared to pass all information to webpage
     context = {'validSession':False, 'username':request.user.username, 'userdic':AllUsers, 
-    'userlist':rand_three, 'explorescroll':AllTweets, 'tweet':tweet}
-    #'following_len':len(following),'followed_by_len':len(followed_by)}
+    'userlist':rand_three, 'explorescroll':AllTweets, 'tweet':tweet, 
+    'following_len':len(following),'followed_by_len':len(followed_by)}
 
     
     ## Check if a user is logged in
@@ -60,68 +57,5 @@ def index(request):
 
     ### Render the webpage
     return render(request,'Main_Page/index.html', context) 
-    #{'profile_user':profile_user, 'following_len':len(following), 'followed_by_len':len(followed_by)})
-    #, {'following_len':len(following),'followed_by_len':len(followed_by)})
-
-def main(request, username):
-
-     if request.method == "POST":
-        
-        if not request.user.is_authenticated:
-            #If the user tries to POST anything and they are not logged in, redirect them to the login page
-            #at some point look at the thing that will redirect them back to the same page and complete the previous
-            #action once they are logged in
-            return HttpResponseRedirect(reverse('Account:login'))
-
-        if request.POST.get('followProfileSubmit'):
-
-            #Create query
-            #user = current session user
-            #following: is the user profile
-            userToFollow = User.objects.get(username=request.POST['followProfileSubmit'])
-            
-            
-            #make sure relationship does not already exist
-            old_follow = Follow.objects.filter(user=request.user,following=userToFollow)
-            #Check to make sure they are not equal
-            if(userToFollow.username != request.user.username and not old_follow):
-                print("Successfully meet criteria to follow")
-                query = Follow(user=request.user,following=userToFollow)
-                query.save()
-                #save the query
-
-            return HttpResponseRedirect(reverse('Main_Page:index', args=[request.POST['followProfileSubmit']]))
-
-        if request.POST.get('unfollowProfileSubmit'):
-            
-            userToUnfollow = User.objects.get(username=request.POST['unfollowProfileSubmit'])
-
-            #Relationship should already exist.
-            unfollow = Follow.objects.filter(user=request.user,following=userToUnfollow)
-            unfollow.delete()
-       
-            #reload the page and make sure an follow button shows back up
-            return HttpResponseRedirect(reverse('Main_Page:index', args=[request.POST['unfollowProfileSubmit']]))
-    
-        profile_user = get_object_or_404(User,username=username)
-        #How many users is the profile user following
-        following = Follow.objects.filter(user = profile_user)
-        #how many people are following the profile user
-        followed_by = Follow.objects.filter(following=profile_user)
-
-        #if current_user is in followed_by...show unfollow
-
-        #Check to see if we are on the users native profile if they are logged in
-        isNative = False
-        auth_follow = False
-        if request.user.is_authenticated:
-            if request.user.username == profile_user.username:
-                isNative = True
-
-                #Check to see if the authenticated user is already following the user
-                for followed in followed_by:
-                    if request.user == followed.user:
-                        auth_follow = True
-        return render(request,'Main_Page/index.html',{'profile_user':profile_user,'auth_follow':auth_follow, 'following_len':len(following),'followed_by_len':len(followed_by),'isNative':isNative})
 
 
