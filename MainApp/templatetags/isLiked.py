ab0b21cc5192a812511008309c027e934288acae
lmurdock12 <44686596+lmurdock12@users.noreply.github.com>
2021-04-04 15:26:25 -0500

    added logos for retweet and like

--- MainApp/templatetags/isLiked.py
+++ MainApp/templatetags/isLiked.py
@@ -1,14 +1,14 @@
 
 
-# templatetags/isRetweeted.py
+# templatetags/isLiked.py
 
 from django import template
 
 register = template.Library()
 
 @register.filter()
-def isRetweeted(tweet, user):
-    if len(tweet.retweetedTweet.filter(user=user)) == 1:
+def isLiked(tweet, user):
+    if len(tweet.tweet.filter(user=user)) == 1:
         return 1
     else:
         return 0
